e3c47dd4a4694e3e68c2dd61161ec6927bf32d5f
K1-mikaze <K1mikaze@proton.me>
2026-01-27 10:37:27 -0500

    Enforcing Environment Variables
    Enforcing the use of Environment variables to make the program work. if
    any Environment variable is missing the program will throw an Error

--- back-end/src/configuration/environment.ts
+++ back-end/src/configuration/environment.ts
@@ -1,10 +1,15 @@
+const MODE: string | null = String(process.env["ENV_MODE"]) || null;
+const API: number | null = Number(process.env.API_PORT) || null;
+const DATABASE_URL: string | null = String(process.env.DATABASE_URL) || null;
+
+if (MODE == null || API == null || DATABASE_URL == null) {
+  throw Error(
+    "!!!!!!! Environment Variables misconfigured:\n Variables required\n - ENV_MODE\n - API_PORT\n - DATABASE_URL",
+  );
+}
+
 export default {
-  MODE: process.env.ENV_MODE || "development",
-  API: process.env.API_PORT || 3000,
-  DB_NAME: process.env.DB_NAME,
-  DATABASE_URL: process.env.DB_URL,
-  DB_HOST: process.env.DB_HOST || "127.0.0.1",
-  DB_PORT: process.env.DB_PORT || 5432,
-  DB_USER: process.env.DB_USER,
-  DB_PASS: process.env.DB_PASS,
+  MODE: MODE,
+  API: API,
+  DATABASE_URL: DATABASE_URL,
 };
